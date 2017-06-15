330e6b60b5bc7b1125df4b80e542cc184cd89a7f
wangbiaozy <56565283@qq.com>
2017-06-15 11:07:08 +0800

    22

--- js/index.js
+++ js/index.js
@@ -13,24 +13,69 @@ $(function () {
     });
     //立即订购
     $('.submit').on('touchend',function () {
-        window.location.href = '';
+        if($(this).hasClass('active')){
+            window.location.href = '';
+        }
     });
     //用户输入验证交互
     $('#secondInput').on('input',function () {
-        if($(this).val() == $('#firstInput').val()){
+        if($(this).val() == $('#firstInput').val() && $('#firstInput').val() != ''){
+            $('.submit').addClass('active');
             $('#wron').hide();
             $('#yes').show();
-        }else{
+        }else if($('#firstInput').val() == '' && $(this).val() == ''){
             $('#yes').hide();
+            $('#wron').hide();
+            $('.submit').removeClass('active');
+        }
+        else{
+            $('#yes').hide();
+            $('.submit').removeClass('active');
         }
     });
     $('#secondInput').on('blur',function () {
         if($(this).val() != $('#firstInput').val()){
             $('.wraning').fadeIn().delay(800).fadeOut();
             $('#wron').show();
+        }else if($(this).val() == ''){
+            $(this).context.placeholder = '请再次输入您的QQ号码';
         }else{
             $('#wron').hide();
         }
+    });
+    $('#firstInput').on('input',function () {
+        if($(this).val() == $('#secondInput').val() && $('#secondInput').val() != ''){
+            $('.submit').addClass('active');
+            $('#wron').hide();
+            $('#yes').show();
+        }else if($(this).val() != $('#secondInput').val() && $('#secondInput').val() != ''){
+            $('.submit').removeClass('active');
+            $('#wron').show();
+        }else if($(this).val() == ''){
+            $('#yes').hide();
+            $('#wron').hide();
+            $('.submit').removeClass('active');
+        }else if($(this).val() == '' && $('#secondInput').val() == ''){
+            $('#yes').hide();
+            $('#wron').hide();
+            $('.submit').removeClass('active');
+        }else{
+            $('#yes').hide();
+            $('.submit').removeClass('active');
+        }
+    });
+    $('#firstInput').on('blur',function () {
+        if($(this).val() != $('#secondInput').val()){
+
+        }else if($(this).val() == ''){
+            $(this).context.placeholder = '请输入您的QQ号码';
+        }else{
+            $('#wron').hide();
+        }
+    });
+    //input获取焦点隐藏placeholder内容
+    $('input[type=text]').on('focus',function () {
+        $(this).context.placeholder = '';
     });
     //点击加载更多
     $('.more').on('touchend',function () {
